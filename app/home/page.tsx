72d79ef5c6a40df279323d5a110b98719d5900b9
Paulo <pvmg1@hotmail.com>
2024-07-10 15:55:24 -0300

    ajsutado tela inicial

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,21 @@
 'use client'
 
-import { useAuthContext } from "@/context/authContext";
+import { Button } from "@nextui-org/button";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Home() {
-  const {saveUser} = useAuthContext();
+  const router = useRouter();
 
-  console.log(saveUser)
-  
   return (
-    <p>Home</p>
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="mt-40">
+        <h1 className="text-4xl font-bold mb-8">Olá, bem vindo ao Gymbro versão Beta</h1>
+
+        <Button onPress={() => router.push("/home/exercicios")} size="lg" color="primary">
+          <p className="text-xl font-bold">Clique aqui</p>
+        </Button>
+      </div>
+    </main>
   )
 }
